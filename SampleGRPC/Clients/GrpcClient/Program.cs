f36f64a7d3e4eee93ea4772cd87a93662d1e255a
Serif Aydin <serifaydin99@gmail.com>
2021-11-26 16:46:05 +0300

    grpc sample

---
+++ SampleGRPC/Clients/GrpcClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Şerif" };
+
+            //var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequested = new CustomerLokupModel { UserId = 1 };
+
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
+            Console.WriteLine();
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName} {currentCustomer.Age} {currentCustomer.EmailAddress} {currentCustomer.IsAlive}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleGRPC/Services/GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,86 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLokupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Çınar";
+                output.LastName = "Aydın";
+            }
+
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Şerif";
+                output.LastName = "Aydın";
+            }
+
+            else if (request.UserId == 3)
+            {
+                output.FirstName = "Elif";
+                output.LastName = "Aydın";
+            }
+            else
+            {
+                output.FirstName = "Kullanıcı Bulunamadı";
+                output.LastName = "NotFound";
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request,
+            IServerStreamWriter<CustomerModel> responseStream,
+            ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName="Şerif",
+                    LastName="Aydın",
+                    Age=30,
+                    EmailAddress="serif@serif.com",
+                    IsAlive=true
+                },
+                new CustomerModel
+                {
+                    FirstName="Elif",
+                    LastName="Aydın",
+                    Age=30,
+                    EmailAddress="cinar@cinar.com",
+                    IsAlive=false
+                },
+                new CustomerModel
+                {
+                    FirstName="Çınar",
+                    LastName="Aydın",
+                    Age=3,
+                    EmailAddress="cinar@cinar.com",
+                    IsAlive=false
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await Task.Delay(2000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
